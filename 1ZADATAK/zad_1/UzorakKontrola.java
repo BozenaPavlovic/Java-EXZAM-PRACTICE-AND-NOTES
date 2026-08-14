f3de2cdecd5dc9d141636baf0777804b051232d3
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-08-15 00:22:08 +0200

    Add UzorakKontrola class with attributes

---
+++ 1ZADATAK/zad_1/UzorakKontrola.java
@@ -0,0 +1,12 @@
+package zad_1;
+
+public class UzorakKontrola {
+
+    private String oznaka;
+    private String materijal;
+    private String laboratorij;
+    private int temperatura;
+    private int vlaznost;
+    private String pakiranje;
+
+}
